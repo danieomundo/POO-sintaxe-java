0d02d1dd78e75785261ba9cab8508cf0b5689fb8
Dani <daniele.ddsp@gmail.com>
2022-04-10 23:12:42 -0300

    Update Ex02.java

--- Ex02.java
+++ Ex02.java
@@ -4,3 +4,33 @@ Escreva um programa que efetue a leitura de três valores reais R, S e T, imprim
 
 */
 
+import java.util.Scanner;
+
+public class Ex02 {
+    
+    public static void main(String[] args) {
+        
+    System.out.println("Digite 3 numeros: ");
+    Scanner leia = new Scanner(System.in);
+    
+    float R = leia.nextFloat();
+    float S = leia.nextFloat();
+    float T = leia.nextFloat(); 
+    float maiorNum;
+    
+    if ((R > S) && (R > T)){
+        maiorNum=R;
+    }
+    
+    if ((S > R) && (S > T)){
+        maiorNum=S;
+    }
+    else {
+        maiorNum=T;
+    }
+    
+    System.out.println("O maior num eh " + maiorNum);
+        
+    }
+      
+}
