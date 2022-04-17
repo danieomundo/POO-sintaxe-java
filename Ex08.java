451f2d2c521f269230b5934e4968a58ef04c66a6
Dani <daniele.ddsp@gmail.com>
2022-04-17 03:49:39 -0300

    Create Ex08.java

---
+++ Ex08.java
@@ -0,0 +1,21 @@
+/*
+
+Enunciado:
+
+Um quadrado mágico é uma matriz quadrada, de valores inteiros, 
+na qual a soma dos elementos de cada linha, a soma dos elementos 
+de cada coluna e a soma dos elementos da diagonal primária e da
+diagonal secundária são iguais. 
+
+A matriz quadrada de ordem 4 abaixo é um exemplo de quadrado mágico, 
+no qual todas as somas são iguais a 34:
+
+4  | 14 | 15 | 1
+9  | 7  | 6  | 12
+5  | 11 | 10 | 8
+16 | 2  | 3  | 13
+
+Desenvolva um programa que leia do usuário uma matriz quadrada de ordem n 
+e imprima se a matriz é um quadrado mágico ou não.
+
+*/
